75f9f526410f153df5d4ad2bf0f976dc7ee7c427
sachinbijalwan <2015csb1027@iitrpr.ac.in>
2019-04-09 21:12:17 +0100

    Navigation drawer problems fixed

--- app/src/main/java/groupfour/software/bikerentalapplication/user/MapUser.java
+++ app/src/main/java/groupfour/software/bikerentalapplication/user/MapUser.java
@@ -1,7 +1,18 @@
 package groupfour.software.bikerentalapplication.user;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,11 +21,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import groupfour.software.bikerentalapplication.R;
+import java.lang.reflect.Field;
 
-public class MapUser extends FragmentActivity implements OnMapReadyCallback {
+import groupfour.software.bikerentalapplication.R;
+import groupfour.software.bikerentalapplication.login.LoginActivity;
+
+public class MapUser extends FragmentActivity implements OnMapReadyCallback,NavigationView.OnNavigationItemSelectedListener {
 
     private GoogleMap mMap;
+    private int activity=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +41,53 @@ public class MapUser extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        BaseActivity b = new BaseActivity();
-        //b.onCreateDrawer();
+        onCreateDrawer();
+  //      BaseActivity b = new BaseActivity();
+       // b.onCreate(savedInstanceState);
+//        b.onCreateDrawer();
     }
 
+    protected void onCreateDrawer(){
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+        toolbar.setTitle(R.string.app_name);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_user);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close){
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+                //ImageButton img=getNavButtonView(toolbar);
 
+                float slideX = drawerView.getWidth() * slideOffset;
+                getNavButtonView(toolbar).setZ(20);
+                getNavButtonView(toolbar).setTranslationX(slideX);
+            }
+        };
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view_user);
+        navigationView.setNavigationItemSelectedListener(this);
+        Drawable icon= getDrawable(R.drawable.ic_motorcycle_black_24dp);
+        getNavButtonView(toolbar).setImageDrawable(icon);
+        getNavButtonView(toolbar).setColorFilter(getResources().getColor(android.R.color.white));
+    }
+    private ImageButton getNavButtonView(Toolbar toolbar) {
+        try {
+            Class<?> toolbarClass = Toolbar.class;
+            Field navButtonField = toolbarClass.getDeclaredField("mNavButtonView");
+            navButtonField.setAccessible(true);
+            ImageButton navButtonView = (ImageButton) navButtonField.get(toolbar);
+
+            return navButtonView;
+        }
+        catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -47,5 +105,73 @@ public class MapUser extends FragmentActivity implements OnMapReadyCallback {
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.ride_cycle) {
+            if(true){
+                Intent i=new Intent(getApplicationContext(), RideCycle.class);
+                i.putExtra("cycle","1");
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(i);
+            }
+            activity=0;
+
+            // Handle the camera action
+        } else if (id == R.id.past_trips) {
+            if(activity!=1){
+                Intent i=new Intent(getApplicationContext(),PastTrips.class);
+                i.putExtra("cycle","0");
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(i);
+            }
+            activity=1;
+
+        } else if (id == R.id.feedback) {
+            if(activity!=2){
+                activity=2;
+                Intent i=new Intent(getApplicationContext(), Feedback.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(i);
+            }
+
+        } else if (id == R.id.rent_cycle) {
+            if(activity!=3){
+                activity=3;
+                Intent i=new Intent(getApplicationContext(), RentCycle.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(i);
+            }
+        }else if (id == R.id.stop_rent) {
+            if(activity!=4){
+                activity=4;
+                Intent i=new Intent(getApplicationContext(), StopRent.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(i);
+            }
+        }else if (id == R.id.logout) {
+            if(activity!=5){
+                activity=5;
+                Intent i=new Intent(getApplicationContext(), LoginActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(i);
+            }
+        }else if (id == R.id.user_map) {
+            if(activity!=6){
+                activity=6;
+                Intent i=new Intent(getApplicationContext(), MapUser.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(i);
+            }
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_user);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
