98dbbe8b5cad2b944c067f0fbc41ab20f23157b5
sachinbijalwan <2015csb1027@iitrpr.ac.in>
2019-04-16 12:24:02 +0100

    minor changes to ui

--- app/src/main/java/groupfour/software/bikerentalapplication/login/ForgotPass.java
+++ app/src/main/java/groupfour/software/bikerentalapplication/login/ForgotPass.java
@@ -8,8 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import groupfour.software.bikerentalapplication.R;
+
 
 public class ForgotPass extends AppCompatActivity {
 
--- app/src/main/java/groupfour/software/bikerentalapplication/user/RentCycle.java
+++ app/src/main/java/groupfour/software/bikerentalapplication/user/RentCycle.java
@@ -1,6 +1,14 @@
 package groupfour.software.bikerentalapplication.user;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
 import groupfour.software.bikerentalapplication.R;
 public class RentCycle extends BaseActivity {
 
@@ -9,5 +17,15 @@ public class RentCycle extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rent_cycle);
         onCreateDrawer();
+        String text="" // Whatever you need to encode in the QR code
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,200,200);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            imageView.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
     }
 }
