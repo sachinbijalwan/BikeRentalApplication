acc1de1e158ca339b5abd14c2c077514d757fbe6
Aditya Gupta <adityagupta1089@gmail.com>
2019-04-25 14:42:50 +0530

    Fix Constants

--- app/src/main/java/groupfour/software/bikerentalapplication/Utility/Constants.java
+++ app/src/main/java/groupfour/software/bikerentalapplication/Utility/Constants.java
@@ -1,22 +1,29 @@
 package groupfour.software.bikerentalapplication.Utility;
 
 public class Constants {
-    public static  final String IPSERVER = "https://csl456-bike-rental-app.herokuapp.com" ;
-    public static final String LOCATION = "location" ;
+    /* API Server */
+    public static final String IPSERVER = "https://csl456-bike-rental-app.herokuapp.com";
 
-    public static final String RIDE_START = "ride/start" ;
-    public static final String RIDE_END = "ride/end" ;
-    public static final String CYCLE = "cycle";
+    /* API URLs */
+    public static final String LOCATION        = "location";
+    public static final String RIDE_START      = "ride/start";
+    public static final String RIDE_END        = "ride/end";
+    public static final String CYCLE           = "cycle";
     public static final String CHANGE_LOCATION = "changeLocation";
-    public static final String PERSON="person";
-    public static final String COMPLAINTS="complaint";
-    public static final String USER="user";
-    public  static final String RIDE="ride";
-    public static final String SESSION="session";
-    public static final String STORED_ID="ID";
-    public static final String STORED_USERNAME="USERNAME";
-    public static final String STORED_EMAIL="EMAIL";
-    public static final String STORED_ACCESS_TOKEN="ACCESS_TOKEN";
-    public static final String STORED_ROLE="ROLE";
-    public static final String PREFERENCES="shared";
+    public static final String PERSON          = "person";
+    public static final String COMPLAINTS      = "complaint";
+    public static final String USER            = "user";
+    public static final String RIDE            = "ride";
+    public static final String SESSION         = "session";
+
+    /* Stored */
+    public static final String STORED_ID           = "ID";
+    public static final String STORED_USERNAME     = "USERNAME";
+    public static final String STORED_EMAIL        = "EMAIL";
+    public static final String STORED_ACCESS_TOKEN = "ACCESS_TOKEN";
+    public static final String STORED_ROLE         = "ROLE";
+    public static final String PREFERENCES         = "shared";
+
+    /* App Permissions */
+    public static final int REQUEST_MAP_PERMISSIONS = 1;
 }
